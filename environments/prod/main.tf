086ee7ae0678e5fdd33e521fb4bbcabf4343c343
XxxKMSxxX <kms1342152@gmail.com>
2024-07-13 21:19:08 +0900

    initial commit

---
+++ environments/prod/main.tf
@@ -0,0 +1,4 @@
+module "ecr" {
+  source      = "../../modules/ecr"
+  repository_name = var.repository_name
+}
---
+++ environments/prod/variables.tf
@@ -0,0 +1,5 @@
+variable "repository_name" {
+  description = "The name of the ECR repository"
+  type        = string
+  default     = "Cryptra-Collector"
+}
