65009337235b25d5514c611d4b8963f2d0e4dd1c
XxxKMSxxX <kms1342152@gmail.com>
2024-07-15 17:28:24 +0900

    フォーマットを修正

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -3,11 +3,11 @@ resource "aws_ecs_cluster" "this" {
 }
 
 resource "aws_launch_configuration" "ecs" {
-  name          = "${var.project_name}-launch-configuration"
-  image_id      = data.aws_ami.ecs.id
-  instance_type = var.instance_type
+  name                 = "${var.project_name}-launch-configuration"
+  image_id             = data.aws_ami.ecs.id
+  instance_type        = var.instance_type
   iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
-  user_data = <<-EOF
+  user_data            = <<-EOF
               #!/bin/bash
               echo ECS_CLUSTER=${aws_ecs_cluster.this.name} >> /etc/ecs/ecs.config
               EOF
@@ -26,7 +26,7 @@ data "aws_ami" "ecs" {
 }
 
 resource "aws_iam_role" "ecs_instance_role" {
-  name = "${var.project_name}-ecs-instance-role"
+  name               = "${var.project_name}-ecs-instance-role"
   assume_role_policy = <<-EOF
     {
       "Version": "2012-10-17",
