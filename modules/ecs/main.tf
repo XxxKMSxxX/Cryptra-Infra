7f22162b25bd095ed7497df269e1ccdef2ca3619
XxxKMSxxX <kms1342152@gmail.com>
2024-07-15 22:44:50 +0900

    iam_roleを追加

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -231,6 +231,7 @@ resource "aws_ecs_service" "this" {
   task_definition = each.value.arn
   desired_count   = 1
   launch_type     = "EC2"
+  iam_role        = aws_iam_role.ecs_service_linked_role.arn
 
   load_balancer {
     target_group_arn = aws_lb_target_group.app.arn
