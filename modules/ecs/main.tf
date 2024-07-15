88555b9b101c619d4f3dede2a62d863e51c95579
XxxKMSxxX <kms1342152@gmail.com>
2024-07-15 22:49:03 +0900

    一度リソースを削除してみる

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -166,64 +166,6 @@ resource "aws_lb_listener" "app" {
   }
 }
 
-resource "aws_iam_role" "ecs_service_linked_role" {
-  name               = "${var.project_name}-ecs-service-role"
-  assume_role_policy = <<-EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "ecs.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
-        }
-      ]
-    }
-  EOF
-}
-
-resource "aws_iam_policy" "ecs_custom_service_policy" {
-  name        = "${var.project_name}-ecs-service-policy"
-  description = "Custom policy for ECS service role"
-  policy      = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "ecs:CreateCluster",
-          "ecs:DeleteCluster",
-          "ecs:DeregisterContainerInstance",
-          "ecs:DiscoverPollEndpoint",
-          "ecs:Poll",
-          "ecs:RegisterContainerInstance",
-          "ecs:StartTelemetrySession",
-          "ecs:Submit*",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:Describe*",
-          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-          "elasticloadbalancing:Describe*",
-          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource": "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_service_linked_role_attachment" {
-  role       = aws_iam_role.ecs_service_linked_role.name
-  policy_arn = aws_iam_policy.ecs_custom_service_policy.arn
-}
-
 resource "aws_ecs_service" "this" {
   for_each        = aws_ecs_task_definition.ecs_task_definitions
   name            = "${var.project_name}-${each.key}-service"
@@ -231,7 +173,6 @@ resource "aws_ecs_service" "this" {
   task_definition = each.value.arn
   desired_count   = 1
   launch_type     = "EC2"
-  iam_role        = aws_iam_role.ecs_service_linked_role.arn
 
   load_balancer {
     target_group_arn = aws_lb_target_group.app.arn
