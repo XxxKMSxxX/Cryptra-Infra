a6c60169e470cdf41ea22aae91ccd731516c6b78
k6s <31524505+XxxKMSxxX@users.noreply.github.com>
2024-07-29 19:18:42 +0900

    タスクロールを設定 (#60)

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -51,6 +51,44 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecs_task_policy" {
+  name        = "${var.project_name}-ecs-task-policy"
+  description = "Policy for ECS tasks to access Kinesis streams"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "kinesis:*",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_task_policy.arn
+}
+
 resource "aws_autoscaling_group" "ecs" {
   launch_configuration = aws_launch_configuration.ecs.id
   min_size             = 1
@@ -70,14 +108,18 @@ resource "aws_cloudwatch_log_group" "ecs_log_group" {
   retention_in_days = 1
 }
 
+
+
 resource "aws_ecs_task_definition" "ecs_task_definitions" {
   for_each = {
     for task in local.tasks :
     lower("${task.exchange}-${task.contract_type}-${task.symbol}") => task
   }
 
-  family       = "${var.project_name}-${each.key}-task"
-  network_mode = "bridge"
+  family        = "${var.project_name}-${each.key}-task"
+  network_mode  = "bridge"
+  task_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
   container_definitions = jsonencode([
     {
       name      = "app"
