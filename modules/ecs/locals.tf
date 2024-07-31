8c6b2a0908e77a0d082233f4b427d00bc009dcf0
k6s <31524505+XxxKMSxxX@users.noreply.github.com>
2024-07-31 13:46:20 +0900

    ポート修正 (#73)
    * ポート修正
    
    * main.tf を更新
    
    * locals.tf を更新
    
    * main.tf を更新

--- modules/ecs/locals.tf
+++ modules/ecs/locals.tf
@@ -1,11 +1,13 @@
 locals {
+  base_port = 8080
   tasks = flatten([
     for exchange_name, exchange in var.collects : [
       for contract_type, symbols in exchange : [
-        for symbol in symbols : {
+        for i, symbol in symbols : {
           exchange      = exchange_name,
           contract_type = contract_type,
-          symbol        = symbol
+          symbol        = symbol,
+          host_port     = local.base_port + i
         }
       ]
     ]
