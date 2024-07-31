eb5ca2d8ef0ba5da4d872000d71aa1a2b07ad3b5
k6s <31524505+XxxKMSxxX@users.noreply.github.com>
2024-07-31 15:12:29 +0900

    locals.tf を更新

--- modules/ecs/locals.tf
+++ modules/ecs/locals.tf
@@ -1,13 +1,13 @@
 locals {
   base_port = 8080
   tasks = flatten([
-    for exchange_name, exchange in var.collects : [
-      for contract_type, symbols in exchange : [
-        for i, symbol in symbols : {
+    for exchange_idx, exchange_name in keys(var.collects) : [
+      for contract_idx, contract_type in keys(var.collects[exchange_name]) : [
+        for symbol_idx, symbol in enumerate(var.collects[exchange_name][contract_type]) : {
           exchange      = exchange_name,
           contract_type = contract_type,
           symbol        = symbol,
-          host_port     = local.base_port + i
+          host_port     = local.base_port + exchange_idx * 10000 + contract_idx * 1000 + symbol_idx
         }
       ]
     ]
