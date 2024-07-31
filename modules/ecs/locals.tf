eac39e532aae907fbe1eb029ed6446d484ed493c
k6s <31524505+XxxKMSxxX@users.noreply.github.com>
2024-07-31 16:03:44 +0900

    locals.tf を更新

--- modules/ecs/locals.tf
+++ modules/ecs/locals.tf
@@ -3,12 +3,12 @@ locals {
   exchange_names = keys(var.collects)
   tasks = flatten([
     for exchange_idx in range(length(local.exchange_names)) : [
-      for contract_type in keys(var.collects[local.exchange_names[exchange_idx]]) : [
-        for symbol_idx in range(length(var.collects[local.exchange_names[exchange_idx]][contract_type])) : {
+      for contract_idx in range(length(keys(var.collects[local.exchange_names[exchange_idx]]))) : [
+        for symbol_idx in range(length(var.collects[local.exchange_names[exchange_idx]][keys(var.collects[local.exchange_names[exchange_idx]])[contract_idx]])) : {
           exchange      = local.exchange_names[exchange_idx],
-          contract_type = contract_type,
-          symbol        = var.collects[local.exchange_names[exchange_idx]][contract_type][symbol_idx],
-          host_port     = local.base_port + exchange_idx * 10000 + find(keys(var.collects[local.exchange_names[exchange_idx]]), contract_type) * 1000 + symbol_idx
+          contract_type = keys(var.collects[local.exchange_names[exchange_idx]])[contract_idx],
+          symbol        = var.collects[local.exchange_names[exchange_idx]][keys(var.collects[local.exchange_names[exchange_idx]])[contract_idx]][symbol_idx],
+          host_port     = local.base_port + exchange_idx * 10000 + contract_idx * 1000 + symbol_idx
         }
       ]
     ]
