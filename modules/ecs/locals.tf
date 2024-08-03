0a4eae9595c25eeba7b6d1ee5a50d6103505e2eb
k6s <31524505+XxxKMSxxX@users.noreply.github.com>
2024-08-03 10:55:38 +0900

    Feature/ヘルスチェックの改善 (#101)
    * タスク定義等の修正
    
    * インデックスの扱いを見直し
    
    * リバイス
    
    * aws_lb_listenerの定義修正
    
    * 環境変数を追加
    
    * health_checkにポート番号の指定を追加
    
    * main.tf を更新
    
    * main.tf を更新
    
    * variables.tf を更新
    
    * locals.tf を更新
    
    * locals.tf を更新
    
    * locals.tf を更新
    
    * locals.tf を更新
    
    * main.tf を更新
    
    * main.tf を更新
    
    * main.tf を更新
    
    * main.tf を更新
    
    * host_portを指定
    
    * host portの指定を削除

--- modules/ecs/locals.tf
+++ modules/ecs/locals.tf
@@ -3,20 +3,20 @@ locals {
     for exchange, contracts in var.collects : [
       for contract, symbols in contracts : [
         for symbol in symbols : {
-          exchange      = exchange,
-          contract      = contract,
-          symbol        = symbol
+          exchange = exchange,
+          contract = contract,
+          symbol   = symbol
         }
       ]
     ]
   ])
 
   collects = {
-    for collect in local.raw_collects : 
+    for collect in local.raw_collects :
     lower("${collect.exchange}-${collect.contract}-${collect.symbol}") => {
-      exchange      = collect.exchange,
-      contract      = collect.contract,
-      symbol        = collect.symbol
+      exchange = collect.exchange,
+      contract = collect.contract,
+      symbol   = collect.symbol
     }
   }
 }
